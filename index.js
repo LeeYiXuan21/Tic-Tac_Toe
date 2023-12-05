4c1c7ada4da3c7bc7e428dc7f9f2a87b9ae4bade
lee yi xuan <s10243281@connect.np.edu.sg>
2023-12-05 22:01:51 +0800

    code winning conditions

--- index.js
+++ index.js
@@ -12,4 +12,15 @@ window.addEventListener('DOMContentLoaded', () => {
     const PLAYERO_WON = 'PLAYERO_WON';
     const TIE = 'TIE';
 
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
     
